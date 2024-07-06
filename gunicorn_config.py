5c925039dd5b6a4422696aa944437285e9efd5be
Prince Sachdeva <147474864+scriptforprince7@users.noreply.github.com>
2024-07-06 12:10:14 +0530

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
-bind = "unix:/python/panipat-deploy/ecomproj.sock"
+bind = "unix:/python/VRH/ecomproj.sock"
 workers = 3
-chdir = "/python/panipat-deploy/"
+chdir = "/python/VRH/"
 module = "ecomproj.wsgi:application"
 user = "root"
 group = "root"
